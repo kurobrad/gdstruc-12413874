e41299e6027ac3a5a13f52e4d094181e96dafbcf
bradley <johnbradley.go@benilde.edu.ph>
2025-11-23 17:33:25 +0800

    Added Quiz 1

--- Quiz 1/src/Main.java
+++ Quiz 1/src/Main.java
@@ -1,13 +1,68 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    public static void main(String[] args) {
+
+        int[] numbers = new int[10];
+        numbers[0] = 35;
+        numbers[1] = 8;
+        numbers[2] = 3;
+        numbers[3] = 133;
+        numbers[4] = 25;
+        numbers[5] = 30;
+        numbers[6] = 400;
+        numbers[7] = 1;
+        numbers[8] = 37;
+        numbers[9] = -40;
+
+        System.out.println("Before the selection sort:");
+        printArrayElements(numbers);
+
+        bubbleSort(numbers);
+
+        System.out.println("\n\nAfter selection sort:");
+        printArrayElements(numbers);
+    }
+
+    private static void bubbleSort(int[] arr)
+    {
+        for (int lastSortedIndex = arr.length - 1; lastSortedIndex > 0; lastSortedIndex--)
+        {
+            for (int i = 0; i < lastSortedIndex; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+    }
+
+    private static void selectionSort(int[] arr)
+    {
+        for (int lastSortedIndex = arr.length - 1; lastSortedIndex > 0; lastSortedIndex--)
+        {
+            int largestIndex = 0;
+
+            for (int i = 1; i <= lastSortedIndex; i++)
+            {
+                if (arr[i] > arr[largestIndex])
+                {
+                    largestIndex = i;
+                }
+            }
+
+            int temp = arr[lastSortedIndex];
+            arr[lastSortedIndex] = arr[largestIndex];
+            arr[largestIndex] = temp;
+        }
+    }
+
+    private static void printArrayElements(int[] arr)
+    {
+        for (int j : arr) {
+            System.out.println(j);
+        }
     }
 }
